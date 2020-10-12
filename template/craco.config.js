f8c095eaa4af16e3ce152b9b499cc6eebd057814
chanonpingpong <chanon.pingpong@gmail.com>
2020-10-12 14:37:05 +0700

    🏓 add alias for typescript

--- template/craco.config.js
+++ template/craco.config.js
@@ -1,7 +1,19 @@
+const CracoAlias = require('craco-alias')
+
 module.exports = {
   style: {
     postcss: {
       plugins: [require('postcss-import'), require('tailwindcss'), require('autoprefixer')],
     },
   },
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        baseUrl: './src',
+        tsConfigPath: './tsconfig.extend.json',
+      },
+    },
+  ],
 }
